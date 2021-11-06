10c7aacfee634cd7f801937019207de0259babeb
LeoJit_15 <76003870+LeoJit@users.noreply.github.com>
2021-11-06 19:41:24 +0530

    Removed non-existent directory.

--- football_website/views.py
+++
@@ -1,7 +0,0 @@
-#from django.http import HttpRequest
-# from django.template import loader
-from django.shortcuts import render
-import json
-
-def index(request):
-    return render(request, 'index.html')
